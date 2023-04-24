ed2127f78e3abf64d6a708a9363a8b12ba60d802
Sebastián Arroniz <sebastianarroniz55@gmail.com>
2023-04-23 21:18:42 -0600

    changes

---
+++ src/sql/default-values.sql
@@ -0,0 +1,47 @@
+INSERT INTO role (id, name)
+VALUES 
+(1, 'Administrador'),
+(2, 'Profesor'),
+(1, 'Estudiante'),
+(1, 'Asistente')
+GO
+
+INSERT INTO period_type (id, name)
+VALUES
+(1, 'Anual'),
+(2, 'Semestral'),
+(3, 'Trimestral'),
+(4, 'Cuatrimestral'),
+(5, 'Bimestral')
+GO
+
+INSERT INTO period_status (id, name)
+VALUES
+(1, 'Creado'),
+(2, 'En progreso'),
+(3, 'Finalizado')
+GO
+
+INSERT INTO period_status (id, name)
+VALUES
+(1, 'Creado'),
+(2, 'En progreso'),
+(3, 'Finalizado')
+GO
+
+INSERT INTO curriculum_status (id, name)
+VALUES
+(1, 'Creación'),
+(2, 'Activo'),
+(3, 'Cerrado')
+GO
+
+INSERT INTO day (id, name)
+VALUES
+(1, 'Lunes'),
+(2, 'Martes'),
+(3, 'Miércoles'),
+(4, 'Jueves'),
+(5, 'Viernes'),
+(6, 'Sábado')
+GO
