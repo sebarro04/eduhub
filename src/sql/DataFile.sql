bbcdb498c0d724d34d6a7840d1de561b3f60cd9a
GeraldNCH <99829363+GeraldNCH@users.noreply.github.com>
2023-04-29 17:23:37 -0600

    DataFile update

--- src/sql/DataFile.sql
+++ src/sql/DataFile.sql
@@ -19,11 +19,34 @@ DELETE FROM school
 */
 /*CREACION DE TABLAS DE ACUERDO A ORDEN Y PRIORIDA PARA FK'S*/
 
-/*PERIODOS*/
+/*PERIODOS CORRIDO*/
 INSERT INTO period (period_type_id, start_date, end_date, period_status_id)
 VALUES (2, '2023-07-06', '2023-11-22', 1), 
 	   (2, '2024-02-04', '2024-06-01', 1),
 	   (3, '2023-08-01', '2023-10-27', 1)
 
-/*ESCUELAS*/
-INSERT INTO school (
+/*ESCUELAS CORRIDO*/
+INSERT INTO school (school.id, school.name, school.email, school.phone_number, school.director_id)
+VALUES ('IC', 'Escuela de Ingenieria en Computacion', 'escuelacomputacion@itc.ac.cr', 83440015, 1),
+	   ('CI', 'Escuela de Ciencias del Idioma', 'escuelaCienciasIdioma@itc.ac.cr', 80457790, 2),
+	   ('MA', 'Escuela de Matematica', 'escuelaMatematica@itc.ac.cr', 89920010, 3),
+	   ('SE', 'Escuela de Ciencias Sociales', 'escuelaCienciasSociales@itc.ac.cr', 84448052, 4),
+	   ('FH', 'Escuela de Centros de Formacion Humanistica', 'escuelaCFH@itc.ac.cr', 86651740, 5)
+
+/*CURSOS CORRIDO*/
+INSERT INTO course (course.id, course.name, course.period_type_id, course.credits, course.school_id, course.class_hours_week, course.description)
+VALUES ('0202', 'Ingles basico', 2, 2, 'CI', 4, 'Curso de Ingles Basico'),
+	   ('0101', 'Matematica General', 2, 2, 'CI', 4, 'Curso de '),
+	   (),
+	   (),
+	   (),
+	   (),
+	   (),
+	   (),
+	   (),
+	   (),
+	   (),
+	   (),
+	   (),
+	   (),
+	   ()
