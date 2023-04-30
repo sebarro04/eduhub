987b21602a97b2b649712fd12886bf6d17f9304a
GeraldNCH <99829363+GeraldNCH@users.noreply.github.com>
2023-04-29 21:41:06 -0600

    Changes

--- src/sql/DataFile.sql
+++ src/sql/DataFile.sql
@@ -35,17 +35,45 @@ VALUES ('IC', 'Escuela de Ingenieria en Computacion', 'escuelacomputacion@itc.ac
 
 /*CURSOS CORRIDO*/
 INSERT INTO course (course.id, course.name, course.period_type_id, course.credits, course.school_id, course.class_hours_week, course.description)
-VALUES ('0202', 'Ingles basico', 2, 2, 'CI', 4, 'Curso de Ingles Basico'),
-	   ('0101', 'Matematica General', 2, 2, 'MA', 4, 'Curso de Matematica General'),
-	   ('1106', 'Comunicacion escrita', 2, 2, 'CI', 4, 'Curso de Comunicacion Escrita'),
-	   ('1400', 'FOC', 2, 3, 'IC', 4, 'Curso de FOC'),
-	   ('1802', 'Introduccion a la programacion', 2, 3, 'IC', 4, 'Intro'),
-	   ('1803', 'Taller de programacion', 2, 3, 'IC', 4, 'Curso de Taller'),
-	   ('1403', 'Matematica Discreta', 2, 4, 'MA', 4, 'Curso de MD'),
-	   ('1100', 'Actividad Cultural', 2, 0, 'SE', 2, 'Actividad Cultural 1'),
-	   ('1230', 'Ingles 1', 2, 2, 'CI', 4, 'Curso de Ingles 1'),
-	   ('1000', 'CFH', 3, 0, 'FH', 4, 'CFH'),
+VALUES ('0202', 'Ingles basico', 2, 2, 'CI', 4, 'Curso de Ingles Basico'), 
+	   ('0101', 'Matematica General', 2, 2, 'MA', 4, 'Curso de Matematica General'), 
+	   ('1106', 'Comunicacion escrita', 2, 2, 'CI', 4, 'Curso de Comunicacion Escrita'), 
+	   ('1400', 'FOC', 2, 3, 'IC', 4, 'Curso de FOC'), 
+	   ('1802', 'Introduccion a la programacion', 2, 3, 'IC', 4, 'Intro'), 
+	   ('1803', 'Taller de programacion', 2, 3, 'IC', 4, 'Curso de Taller'), 
+	   ('1403', 'Matematica Discreta', 2, 4, 'MA', 4, 'Curso de MD'), 
+	   ('1100', 'Actividad Cultural', 2, 0, 'SE', 2, 'Actividad Cultural 1'), 
+	   ('1230', 'Ingles 1', 2, 2, 'CI', 4, 'Curso de Ingles 1'), 
+	   ('1000', 'CFH', 3, 0, 'FH', 4, 'CFH'), 
 	   ('2001', 'Estructuras de Datos', 2, 4, 'IC', 4, 'ED'),
-	   ('2101', 'Programacion Orientada a Objetos', 2, 3, 'IC', 4, 'POO'),
-	   ('3101', 'Arquitectura de Computadoras', 2, 4, 'IC', 4, 'Curso de '),
-	   ('1102', 'Calculo Diferencial e Integral', 2, 4, 'IC', 4, 'CDI')
+	   ('2101', 'Programacion Orientada a Objetos', 2, 3, 'IC', 4, 'POO'), 
+	   ('3101', 'Arquitectura de Computadoras', 2, 4, 'IC', 4, 'Curso de '), 
+	   ('1102', 'Calculo Diferencial e Integral', 2, 4, 'IC', 4, 'CDI') 
+
+/*CARRERA CORRIDO*/
+INSERT INTO career (career.name, career.school_id, career.description)
+VALUES ('Ingenieria en Computación', 'IC', '¡Bienvenido a la carrer de Ingenieria en computacion!')
+
+/*CURRICULUM CORRIDO*/
+INSERT INTO curriculum (curriculum.id, curriculum.curriculum_status_id, curriculum.career_id, curriculum.creation_date, curriculum.activation_date, curriculum.finish_date)
+VALUES('0001', 2, 2, '2016-01-01', '2016-02-01', '2026-01-01')
+
+/*CURSOS ASOCIADOS A UN PLAN CORRIDO*/
+INSERT INTO curriculum_course (curriculum_course.curriculum_id, curriculum_course.course_id)
+VALUES ('0001', '0202'), ('0001', '0101'), ('0001', '1106'), ('0001', '1400'), ('0001', '1802'), ('0001', '1803'), ('0001', '1403'),
+	   ('0001', '1100'), ('0001', '1230'), ('0001', '1000'), ('0001', '2001'), ('0001', '2101'), ('0001', '3101'), ('0001', '1102')
+
+/*CURSOS CON DEPENDENCIA CORRIDO*/
+INSERT INTO curriculum_course_dependency (curriculum_course_prerequisite.curriculum_id, curriculum_course_prerequisite.course_id, curriculum_course_prerequisite.course_dependency_id)
+VALUES ('0001', '1803', '1802'), ('0001', '2001', '2101')
+
+/*PREREQUISITOS ENTRE CURSOS CORRIDO*/
+INSERT INTO curriculum_course_prerequisite (curriculum_course_prerequisite.curriculum_id, curriculum_course_prerequisite.course_id, curriculum_course_prerequisite.course_prerequisite_id)
+VALUES ('0001', '2001', '1802'), ('0001', '2001', '1803'),
+	   ('0001', '2101', '1802'), ('0001', '2101', '1803'),
+	   ('0001', '3101', '1400'),
+	   ('0001', '1102', '0101')
+SELECT * FROM period
+/*CLASES ASOCIADAS A LOS CURSOS*/
+INSERT INTO class (class.course_id, class.period_id, class.professor_id, class.max_student_capacity)
+VALUES ('0202', 8, 'professor-0202', 25)
