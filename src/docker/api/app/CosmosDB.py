f8b0fc06aa197276fb274953bc7f204886a98b59
GeraldNCH <99829363+GeraldNCH@users.noreply.github.com>
2023-04-30 21:45:35 -0600

    Changes

--- src/docker/api/app/CosmosDB.py
+++ src/docker/api/app/CosmosDB.py
@@ -1,6 +1,20 @@
 from cassandra.cluster import Cluster
-from flask import Flask, jsonify, request
+from cassandra.auth import PlainTextAuthProvider
+import config as cfg
 
+def insert_user_log(user_id, user_action):
+    # Authenticate and connect to the cluster
+    auth_provider = PlainTextAuthProvider(username=cfg.config['username'], password=cfg.config['password'])
+    cluster = Cluster([cfg.config['contactPoint']], port=cfg.config['port'], auth_provider=auth_provider, ssl_context=None)
+    session = cluster.connect(cfg.config['keyspace'])
+    
+    # Insert user log document into 'userlogs' collection
+    query = "INSERT INTO userlogs (user_id, user_action) VALUES (%s, %s)"
+    session.execute(query, (user_id, user_action))
+    
+    # Close the session and cluster
+    session.shutdown()
+    cluster.shutdown()
 
-
-
+if __name__ == '__main__':
+    print(insert_user_log('123', 'User login into the system'))
---
+++ src/docker/api/app/config.py
@@ -0,0 +1,7 @@
+config = {
+    'username': 'tfex-cosmos-db-98560',
+    'password': 'jFaGMKu4HG5lBSmYsQmB01hBGhBtXziNoAqDTiNOMuLvmSXEvxfSEUkmVNLfX2OFR1mRvj8Q5GxwACDbZH1qrg==',
+    'contactPoint': 'tfex-cosmos-db-98560.cassandra.cosmos.azure.com',
+    'port': '10350',
+    'keyspace': 'tfex-cosmos-cassandra-keyspace'
+}
